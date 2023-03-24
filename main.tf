a60ae41989769ee13da21a17c4321b2edd06f664
sravani940 <126774624+sravani940@users.noreply.github.com>
2023-03-24 11:28:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAY6E6BTLNTJXBG6PB"
-    secret_key = "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"
+  access_key = "Acess key"
+    secret_key = "secret key"
 }
 
 resource "aws_s3_bucket" "user" {
